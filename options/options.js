87a31729ef093168ba55b22605de035e4f751a38
Crowdbotics <team@crowdbotics.com>
2023-09-03 13:53:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-meadow-43212",
+  url: "https://delicate-meadow-43212.botics.co",
+  api: "https://delicate-meadow-43212.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
